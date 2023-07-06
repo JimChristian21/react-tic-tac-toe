330434d99ddaae42581f38b26b821b2286fe44ba
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-07-07 04:18:03 +0800

    Added Square and Board Component and use useState Hook

---
+++ src/components/Square.jsx
@@ -0,0 +1,19 @@
+import { React, useState } from "react";
+
+export default function Square() {
+
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        console.log('clicked!');
+    }
+
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}

057fab6b65503f0a022810b345768a48fb3424d6
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-07-07 04:43:19 +0800

    use useState and correct import React

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,13 @@
-import { React, useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 export default function Square() {
 
     const [value, setValue] = useState(null);
 
     function handleClick() {
-        console.log('clicked!');
+
+        setValue('X');
     }
 
     return (
